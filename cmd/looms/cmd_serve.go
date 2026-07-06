1221587cd4e72f2e2480d985cd7424306d7a83f6
vasu.katravath <vasu.katravath@teradata.com>
2026-07-06 14:19:55 +0530

    feat(agent_runtime): WIP

--- cmd/looms/cmd_serve.go
+++ cmd/looms/cmd_serve.go
@@ -3900,6 +3900,7 @@ func initializeMCPManager(config *Config, logger *zap.Logger) (*mcpManager, erro
 			Command:          serverConfig.Command,
 			Args:             serverConfig.Args,
 			Env:              serverConfig.Env,
+			Headers:          serverConfig.Headers,
 			Transport:        transport,
 			URL:              serverConfig.URL,
 			Headers:          headers,
